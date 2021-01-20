c17490d06946fb7781b610f09b2bd7f179ba8a1d
Tobias Kuppens Groot <tobias.kuppens.groot@ing.com>
2021-01-20 12:00:42 +0100

    fix: accidently commited ui package
    Signed-off-by: Tobias Kuppens Groot <tobias.kuppens.groot@ing.com>

--- packages/flock-ui/build/TestComponent/TestComponent.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { TestComponentProps } from "./TestComponent.types";
-import "./TestComponent.scss";
-export declare const TestComponent: React.FC<TestComponentProps>;
